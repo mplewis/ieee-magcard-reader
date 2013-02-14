df89b02f370a55c1564c8215713077f33134ed7b
Matthew Lewis <matt@mplewis.com>
2013-02-14 15:36:03 -0600

    removed tts functionality because it was silly

--- cardRead.py
+++ cardRead.py
@@ -2,66 +2,53 @@
 
 import sys
 import getpass
-import subprocess
-
 from unixTime import unixTime
-
-# son you better have espeak installed on your unixbox
-def say(text):
-	subprocess.call('espeak "' + text + '"', shell=True)
-	#pass
 
 cardLog = {}
 
 print 'Swipe your U card now.'
 
 while True:
-	raw = getpass.getpass('')
-	swipeTime = unixTime()
-	if raw == "":
-		break
+    raw = getpass.getpass('')
+    swipeTime = unixTime()
+    if raw == "":
+        break
 
-	#print '\t' + raw
-	# track 1 begins with % and ends with ?
-	# track 2 begins with ? and ends with ?
-	start1 = raw.find('%')
-	end1 = raw.find('?')
-	track1 = raw[start1+1:end1]
-	raw2 = raw[end1+1:]
-	start2 = raw2.find(';')
-	end2 = raw2.find('?')
-	track2 = raw2[start2+1:end2]
-	#print '    Track 1: ' + track1
-	data1 = track1.split('^')
-	#print '    Track 2: ' + track2
-	
-	if len(data1) < 6:
-		badDataStr = "That's not a U card."
-		print badDataStr
-		say(badDataStr)
-		continue
+    #print '\t' + raw
+    # track 1 begins with % and ends with ?
+    # track 2 begins with ? and ends with ?
+    start1 = raw.find('%')
+    end1 = raw.find('?')
+    track1 = raw[start1+1:end1]
+    raw2 = raw[end1+1:]
+    start2 = raw2.find(';')
+    end2 = raw2.find('?')
+    track2 = raw2[start2+1:end2]
+    data1 = track1.split('^')
+    
+    if len(data1) < 6:
+        badDataStr = "That's not a U card."
+        print badDataStr
+        say(badDataStr)
+        continue
 
-	idNum = data1[1]
-	if idNum in cardLog:
-		print "You've already swiped in, " + cardLog[idNum]['firstName'] + ' ' + cardLog[idNum]['lastName'] + '!'
-		say("You've already swiped in!")
-		continue
+    idNum = data1[1]
+    if idNum in cardLog:
+        print "You've already swiped in, " + cardLog[idNum]['firstName'] + ' ' + cardLog[idNum]['lastName'] + '!'
+        say("You've already swiped in!")
+        continue
 
-	fullNameReversed = data1[5]
-	#print 'ID:   ' + idNum
-	#print 'Name: ' + fullNameReversed
-	fullNameSplit = fullNameReversed.split(', ')
-	firstName = fullNameSplit[1]
-	lastName = fullNameSplit[0]
-	fullNameProper = firstName + ' ' + lastName
-	fullNameTts = fullNameProper.replace(' ', ', ') + '!'
-	#print 'Name: ' + fullNameProper
+    fullNameReversed = data1[5]
+    fullNameSplit = fullNameReversed.split(', ')
+    firstName = fullNameSplit[1]
+    lastName = fullNameSplit[0]
+    fullNameProper = firstName + ' ' + lastName
+    fullNameTts = fullNameProper.replace(' ', ', ') + '!'
 
-	card = [idNum, lastName, firstName]
-	cardLog[idNum] = {'time': unixTime(), 'lastName': lastName, 'firstName': firstName}
+    card = [idNum, lastName, firstName]
+    cardLog[idNum] = {'time': unixTime(), 'lastName': lastName, 'firstName': firstName}
 
-	print 'Hello, ' + fullNameProper + '!'
-	say('Hello, ' + fullNameTts)
+    print 'Hello, ' + fullNameProper + '!'
 
 print cardLog
 say ('Goodbye!')
